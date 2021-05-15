431de03fea1484d455328ab102f5bdd13d3c8990
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2021-05-15 18:54:42 +0100

    created sidebar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import RecommendedVideos from './components/RecommendedVideos';
 
 import './App.css';
 
@@ -6,6 +8,10 @@ function App() {
   return (
     <div className="app">
       <Header/>
+      <div className="app__page">
+        <Sidebar/>
+        <RecommendedVideos/>
+      </div>
     </div>
   );
 }
---
+++ src/components/RecommendedVideos.js
@@ -0,0 +1,12 @@
+import '../css/RecommendedVideos.css';
+
+
+const RecommendedVideos = () => {
+    return (
+        <div className="recommendedVideos">
+            <h2>Recommended</h2>
+        </div>
+    );
+}
+ 
+export default RecommendedVideos;
